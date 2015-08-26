488d3adcacc7892bf34d3dc57504b23e1a494417
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-26 16:38:17 -0700

    added makeSongAnchor and printing song logic

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,4 +1,5 @@
-$(document).ready(function() {
+// $(document).ready(function() {
+$(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
@@ -11,8 +12,21 @@ $(document).ready(function() {
     $.ajax(url, {
       type: "GET",
       success: function(data) {
-        console.log(data);
+        // console.log(data);
+        var songs = data;
+        console.log(songs[0]);
+        for (i = 0; i < songs.length; i++) {
+          makeSongAnchor(songs[i]);
+        };
       }
     });
   });
+
+  function makeSongAnchor(song) {
+    var anchor = $("<a></a>"); // creates an anchor element
+    anchor.text(song.title);
+    anchor.prop("href", song.url);
+    $("form").after(anchor); // this makes it appear on page
+  }
 });
+
