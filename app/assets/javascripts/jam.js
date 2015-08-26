5def2faa970f22d1d387591c1626f66cd77551a4
Elsa <elsa.moluf@gmail.com>
2015-08-26 16:08:40 -0700

    Rendering results on page as links. Woo.

--- app/assets/javascripts/jam.js
+++ app/assets/javascripts/jam.js
@@ -9,10 +9,29 @@ $(function () {
       type: method,
       success: function(data) {
         console.log(data);
+        renderData(data);
       }
-    });
+    }); // end ajax
   });
-});
+
+  function renderData(data) {
+    for (var i = 0; i < data.length; i++) {
+      var jam = data[i];
+      var artist = jam.artist;
+      var title = jam.title;
+      var url = jam.url;
+      var via = jam.via;
+      var anchor = $("<a></a>");
+      var newline = $("<br>");
+      var listing = (artist + " : " + title + " (via " + via + ")");
+      anchor.text(listing);
+      anchor.prop("href", url);
+      $("body").append(anchor);
+      $("body").append(newline);
+    }
+  } // end renderData
+
+}); // end js file
 
 // var anchor = $("<a></a>"); // new anchor element
 // anchor.text("text on the page");
