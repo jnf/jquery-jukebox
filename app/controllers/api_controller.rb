2a6d39c4e222cd872fa48896ae9b307cd983b017
jnf <jeremy.flores@gmail.com>
2015-08-26 00:00:37 -0700

    Big glut of Rails and API setup.

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,30 @@
+class ApiController < ApplicationController
+  JAM = "http://api.thisismyjam.com/1/search/jam.json"
+
+  def search
+    begin
+      response = HTTParty.get(JAM, query: { "by" => "artist", "q" => params[:artist] })
+      data = setup_data(response)
+      code = :ok
+    rescue
+      data = {}
+      code = :no_content
+    end
+
+    render json: data.as_json, code: code
+  end
+
+  private
+
+  def setup_data(response)
+    jams = response.fetch "jams", {}
+    jams.map do |jam|
+      {
+        via: jam.fetch("via", ""),
+        url: jam.fetch("viaUrl", ""),
+        title: jam.fetch("title", ""),
+        artist: jam.fetch("artist", "")
+      }
+    end
+  end
+end
