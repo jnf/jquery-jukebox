21fccd6851ed58d1a8f0cae31e69e7c769941350
Shanna Chau <shannachau@gmail.com>
2015-08-26 16:59:29 -0700

    Hitting search button multiple times now clears previous elements

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -1,3 +1,30 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(function() {
+  $(".search").click(function() {
+    event.preventDefault();
+
+    $('ul').empty();
+
+    var form = $(this).parent();
+    var url = form.attr("action");
+    var method = form.attr("method");
+
+    $.ajax(url, {
+      type: method,
+      data: { 'artist' : $("#artist").val() },
+      success: function (data) {
+        formatData(data);
+      }
+    });
+  });
+
+  function formatData(data) {
+    for (var i = 0; i < data.length; i++) {
+      var anchor = $('<a></a>');
+      anchor.text(data[i].artist + " - " + data[i].title);
+      anchor.prop('href', data[i].url);
+      $('ul').append(anchor);
+
+      $('a').wrap('<li></li>');
+    }
+  }
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'api#index'
-  get '/search/:artist' => 'api#search', as: 'search'
+  post '/search' => 'api#search', as: 'search'
 end
