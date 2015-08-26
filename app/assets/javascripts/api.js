ce454f99fdcdb016ad944e733586d8fe5492a9d7
Loraine <loraine@lorainekv.com>
2015-08-26 16:35:51 -0700

    Retrieved the input value of the artist field.

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -1,5 +1,28 @@
 $(function() {
   $(".search").click(function(event){
-    // event.preventDefault();
+    event.preventDefault();
+
+    var searchButton = $(this);
+
+    var formTag = searchButton.parent("form");
+
+    var url = formTag.attr("action");
+    var method = formTag.attr("method");
+    var children = formTag.children("#artist")
+    var artistField = children.val()
+
+    // console.log(value)
+
+    // $.ajax(url, {
+    //  type: method,
+    //  success: function (data) {
+    //   console.log(data)
+    //   }
+    // });
+
+    // collect text in the field
+    // assemble the correct url
+    // snag the correct method
+    // hit the controller
   });
 });
