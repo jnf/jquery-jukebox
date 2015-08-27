be1ff957dd764aafdf0a9646f2abdcde5f1b2446
Elia <elgrenier@gmail.com>
2015-08-27 16:01:14 -0700

    added specs for new methods and controller

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe HomeController, type: :controller do
+
+  describe "GET #index is the home page" do
+    it "renders the #index view" do
+      get :index
+
+      expect(response).to render_template("index")
+      expect(response).to have_http_status(200)
+    end
+  end
+end
