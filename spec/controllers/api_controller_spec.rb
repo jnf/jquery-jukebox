3ed7e2f12c909ec2c3e4b02c2f43c5511d08d3fc
Lindsey Reno <lindseyannereno@gmail.com>
2015-08-27 14:18:41 -0700

    Added tests for popular action

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -1,27 +1,53 @@
 require 'rails_helper'
 
 TIMJ_SEARCH = { cassette_name: "TIMJ_search", record: :new_episodes }
+POPULAR = { cassette_name: "Popular", record: :new_episodes }
 
 RSpec.describe ApiController, type: :controller do
-  describe "interacting with the This Is My Jam API", vcr: TIMJ_SEARCH do
-    before :each do
-      get :search, artist: "the knife"
+  describe "interacting with the This Is My Jam API" do
+    describe "searching by artist", vcr: TIMJ_SEARCH do
+      before :each do
+        get :search, artist: "the knife"
+      end
+
+      it "should be successful" do
+        expect(response).to be_ok
+      end
+
+      it "should return a json response object" do
+        expect(response.header['Content-Type']).to include 'application/json'
+      end
+
+      context "the returned json object" do
+        it "has the right keys" do
+          data = JSON.parse response.body
+
+          %w(title artist via url).each do |key|
+            expect(data.map(&:keys).flatten.uniq).to include key
+          end
+        end
+      end
     end
 
-    it "should be successful" do
-      expect(response).to be_ok
-    end
+    describe "exploring popular", vcr: POPULAR do
+      before :each do
+        get :popular
+      end
 
-    it "should return a json response object" do
-      expect(response.header['Content-Type']).to include 'application/json'
-    end
+      it "should be successful" do
+        expect(response).to be_ok
+      end
 
-    context "the returned json object" do
-      it "has the right keys" do
-        data = JSON.parse response.body
-        
-        %w(title artist via url).each do |key|
-          expect(data.map(&:keys).flatten.uniq).to include key
+      it "should return a json response object" do
+        expect(response.header['Content-Type']).to include 'application/json'
+      end
+
+      context "the returned json object" do
+        it "has the right keys" do
+          data = JSON.parse response.body
+          %w(title artist via url).each do |key|
+            expect(data.map(&:keys).flatten.uniq).to include key
+          end
         end
       end
     end
