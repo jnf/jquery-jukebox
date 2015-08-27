24ab95e2a9606c5ed0cabc12a3feeaa641ceb4d8
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-27 14:09:16 -0700

    uncommented and modified specs for :popular action

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -4,7 +4,6 @@ TIMJ_SEARCH = { cassette_name: "TIMJ_search", record: :new_episodes }
 TIMJ_SEARCH_EMPTY = { cassette_name: "TIMJ_no_content", record: :new_episodes }
 
 TIMJ_POPULAR = { cassette_name: "TIMJ_POPULAR", record: :new_episodes }
-# TIMJ_POPULAR_EMPTY = { cassette_name: "TIMJ_POPULAR_EMPTY", record: :new_episodes }
 
 RSpec.describe ApiController, type: :controller do
   describe "Searching for JAMS", vcr: TIMJ_SEARCH do
@@ -50,26 +49,19 @@ RSpec.describe ApiController, type: :controller do
         expect(response).to be_ok
       end
 
-      # it "should return a json response object" do
-      #   expect(response.header['Content-Type']).to include 'application/json'
-      # end
+      it "should return a json response object" do
+        expect(response.header['Content-Type']).to include 'application/json'
+      end
 
-      # context "the returned json object" do
-      #   it "has the right keys" do
-      #     data = JSON.parse response.body
+      context "the returned json object" do
+        it "has the right keys" do
+          data = JSON.parse response.body
           
-      #     %w(title artist via url).each do |key|
-      #       expect(data.map(&:keys).flatten.uniq).to include key
-      #     end
-      #   end
-      # end
+          %w(title artist via url).each do |key|
+            expect(data.map(&:keys).flatten.uniq).to include key
+          end
+        end
+      end
     end
-
-    # describe "no results returned", vcr: TIMJ_SEARCH_EMPTY do
-    #   it "tells me I've got a 204" do
-    #     get :search, artist: "lololololololol"
-    #     expect(response.status).to eq 204
-    #   end
-    # end
   end
 end
