f66780b04ed8e8d034ef879a4f9de805022944b1
jnf <jeremy.flores@gmail.com>
2015-08-27 10:06:04 -0700

    Adds embeds for youtube and vimeo.

--- app/assets/javascripts/jam.js
+++ app/assets/javascripts/jam.js
@@ -4,18 +4,12 @@ $('document').ready(function() {
     var artist = $(this).find('input[name=artist]').val();
     var url    = "/search/" + encodeURI(artist);
 
-    console.log(url);
     if (artist) {
       $.get(url, function(response) {
         var anchors = $.map(response, function(data) {
-          var anchor = $('<a></a>');
-          
-          anchor.prop('href', data["url"]);
-          anchor.text(data["artist"] + ": " + data["title"]);
-
-          return $('<li></li>').html(anchor);
+          var formatter = new Formatter(data);
+          return $('<li></li>').html(formatter.build());
         });
-
 
         $('.links').html(anchors);
       });
@@ -23,16 +17,47 @@ $('document').ready(function() {
   });  
 });
 
-var Formatter = function(data){
+var Formatter = function(data) {
   this.data = data;
 };
 
 Formatter.prototype.build = function() {
-  console.log(this);
-  $('<li></li>').html(this.link_to(data));
+  switch(this.data["via"]) {
+    case "youtube":
+      var code = this.youtube_embed();
+      break;
+    case "vimeo":
+      var code = this.vimeo_embed();
+      break;
+    default:
+      var code = this.link_to();
+  }
+
+  return $('<li></li>').html(code);  
 }
 
-Formatter.prototype.link_to = function(data) {
-
+Formatter.prototype.link_to = function() {
+  var anchor = $('<a></a>');
+  
+  anchor.prop('href', this.data["url"]);
+  anchor.text(this.data["artist"] + ": " + this.data["title"]);
   return anchor;
+}
+
+Formatter.prototype.youtube_embed = function() {
+  var embed_reference = this.data["url"].split("=")[1];
+  var iframe = $('<iframe></iframe>');
+
+  iframe.prop("allowfullscreen","").prop('frameborder', 0);
+  iframe.prop("src", "https://www.youtube.com/embed/" + embed_reference);
+  return iframe;
+}
+
+Formatter.prototype.vimeo_embed = function() {
+  var embed_reference = this.data["url"].split("/").pop();
+  var iframe = $('<iframe></iframe>');
+
+  iframe.prop("allowfullscreen","").prop('frameborder', 0);
+  iframe.prop("src", "https://player.vimeo.com/video/" + embed_reference);
+  return iframe;
 }
