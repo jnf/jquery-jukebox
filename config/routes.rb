03e98040dadf4fd980f3d810fda20fb0487226e8
Lindsey Reno <lindseyannereno@gmail.com>
2015-08-27 14:17:20 -0700

    Added route for popular action in api controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "api#index"
-  get '/search' => 'api#search', as: 'search'
+  get '/search'  => 'api#search',  as: 'search'
+  get '/popular' => 'api#popular', as: 'popular'
 end
