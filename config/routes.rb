71502b2188f0729873c714efacfbbccd888321ce
Kari <kari.lonczak@gmail.com>
2015-08-27 14:29:26 -0700

    Created route for home page view with home#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   controller :api do
     get '/search/:artist', action: 'search'
   end
+
+  root 'home#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
