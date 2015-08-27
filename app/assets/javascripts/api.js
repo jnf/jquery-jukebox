66d744dd3023481dd9ccefd22cfa3772f841455e
Jeri <drvonnjerryxlii@gmail.com>
2015-08-26 18:45:50 -0700

    wip jukebox soz no commits until now but super YAY embed working now n_n

---
+++ app/assets/javascripts/api.js
@@ -0,0 +1,102 @@
+$(function() { // this tricks rails into seeing our code
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var button = $(":submit");
+    var search = $(":text");
+    var searchQuery = search.val();
+    var form = $("form");
+    var method = form.attr("method");
+    var url = form.attr("action");
+
+    $.ajax(url, {
+      type: method,
+      data: { 'artist': searchQuery },
+      success: function(data) {
+        if (data.length > 0)
+          displayResults(data);
+        else
+          rickRoll(searchQuery);
+      }
+    });
+  });
+
+  function displayResults(data) {
+    var body = $('body');
+    if ($('div')) { $('div').remove(); };
+
+    var div = $('<div></div>');
+    for(var i = 0; i < data.length; i++) {
+      var result = data[i];
+      var paragraph = $('<p></p>');
+      var link = $('<a></a>');
+      link.text(result.artist + "'s " + result.title);
+      link.prop('href', result.url);
+      paragraph.append(link);
+      // console.log(result.url.includes("youtube"));
+
+      if (result.url.includes("youtube")) {
+        paragraph.append("<br />");
+        paragraph.append(youtubeEmbed(result.url));
+      }
+
+      div.append(paragraph);
+    }
+    body.append(div);
+  }
+
+  function youtubeEmbed(url) {
+    // "aa".replace("a", "b") // "ba"
+    // https://www.youtube.com/watch?v=j-TSYvSOv6Y
+    url = url.replace("http://", "https://");
+    url = url.replace("watch?v=", "embed/");
+
+    // <iframe width="560" height="315" src="https://www.youtube.com/embed/6rNgCnY1lPg" frameborder="0" allowfullscreen></iframe>
+    var output = '<iframe width="560" height="315" src="';
+    output += url;
+    output += '" frameborder="0" allowfullscreen></iframe>';
+    // return '<iframe width="560" height="315" src="https://www.youtube.com/embed/d6PJdJvMt2U" frameborder="0" allowfullscreen></iframe>';
+    console.log(url);
+    return output;
+  };
+
+  function rickRoll(artist) {
+    var explanationText = "No results were found for " + artist + ". Perhaps you would enjoy one of these musical selections:"
+    var body = $('body');
+    body.append("<p>" + explanationText + "</p>");
+
+    var firstLink = {
+      artist: "The Muppets",
+      title: "Hilarious Muppet Bloopers!",
+      url: "https://www.youtube.com/watch?v=3KANI2dpXLw"
+    };
+
+    var secondLink = {
+      artist: "Chris Hadfield",
+      title: "Space Oddity Cover",
+      url: "https://www.youtube.com/watch?v=KaOC9danxNo"
+    };
+
+    var thirdLink = {
+      artist: "Seremedy",
+      title: "NO ESCAPE",
+      url: "https://www.youtube.com/watch?v=BbTAMzgC7uU"
+    };
+
+    var fourthLink = {
+      artist: "No Brain",
+      title: "넌 내게 반했어",
+      url: "https://www.youtube.com/watch?v=u_CQB1scYuw"
+    };
+
+    var fifthLink = {
+      artist: "Weird Al",
+      title: "Foil",
+      url: "https://www.youtube.com/watch?v=w-0TEJMJOhk"
+    };
+
+
+    var weirdLinks = [firstLink, secondLink, thirdLink, fourthLink, fifthLink];
+
+    displayResults(weirdLinks);
+  }
+});
