d32fb32d851cb182c9b994c437fe907a01ea4569
Loren Wang <wang131.131@gmail.com>
2015-08-27 15:34:50 -0700

    one .js file for both endpoints

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -13,46 +13,64 @@ $(function() {
     //console.log(formTag.hasClass("form"));
 
     var text_box = formTag.children(".text_box");
-  // console.log(text_box.hasClass("text_box"));
+    // console.log(text_box.hasClass("text_box"));
 
     var search_input = text_box.val();
 
-    var url = "/search/" + search_input; // routes to controller then action which 
+    var url = "/search/" + search_input; // routes to controller then action which
 
-    var method = text_box.attr("method");
+    var method = formTag.attr("method");
 
-    var last_child = $('body').children(".results");
+    //var last_child = $('body').children(".results");
     // console.log(searchButton.children());
 
-  $.ajax(url, {
-    type: method,
-    success: function (data) {
+    ajax(url, method);
 
-      if( $('.results').is(':empty') ) {
-        displayData(data);
-      } else {
-        $('.results').empty().append(displayData(data));
+  });
+
+
+  $(".rando").click(function(event) {
+    event.preventDefault();
+
+    var randoInput = $(this);
+
+    var randoForm = randoInput.parents(".button_to");
+
+    var url = "/rando";
+
+    var method = randoForm.attr("method");
+
+    //var last_child = $('body').children(".results");
+
+    ajax(url, method);
+
+  });
+
+  function ajax(url, method) {
+
+    $.ajax(url, {
+      type: method,
+      success: function (data) {
+        if( $('.results').is(':empty')) {
+          displayData(data);
+        } else {
+          $('.results').empty().append(displayData(data));
+        }
       }
-    }
-
-  });
-
-  function displayData(data) {
-    for (var i = 0; i < data.length; i++) {
-      artist_title = data[i].artist + ": " + data[i].title;
-      url = data[i].url;
-
-      var anchor = $('<a></a>');
-      anchor.text(artist_title);
-      anchor.prop('href', url);
-      var p_tag = $('<p></p>');
-      p_tag.append(anchor);
-      $('.results').append(p_tag);
-
+    });
   }
-  return data; //return artist will have console.log(displayData(data)); inside success function to be only the artist
-}
 
-//$('.results').children();
-  });
+    function displayData(data) {
+      for (var i = 0; i < data.length; i++) {
+        artist_title = data[i].artist + ": " + data[i].title;
+        url = data[i].url;
+        var anchor = $('<a></a>');
+        anchor.text(artist_title);
+        anchor.prop('href', url);
+        var p_tag = $('<p></p>');
+        p_tag.append(anchor);
+        $('.results').append(p_tag);
+      }
+      return data; //return artist will have console.log(displayData(data)); inside success function to be only the artist
+    }
 });
