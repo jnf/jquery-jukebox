30c3c59ce690f10d0011355736ef99024503c3ff
Lindsey Reno <lindseyannereno@gmail.com>
2015-08-27 14:15:28 -0700

    Added click event functionality for popular jams; moved showResults function so its available to both click events

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -15,20 +15,38 @@ $(function() {
       }
     });
 
-    function showResults(data) {
-      var list = $("<ul></ul>");
-      $("ul").replaceWith(list);
-      $("body").append(list);
-      for (var i = 0; i < data.length; i++) {
-        var listItem = $("<li></li>");
-        var anchor = $("<a></a>");
-        var result = data[i]
-        anchor.text(result.artist + ": " + result.title);
-        anchor.prop("href", result.url);
-        list.append(listItem);
-        $("li:last").append(anchor);
-      };
-    };
+  });
+
+  $('.popular').click(function(event){
+    event.preventDefault();
+
+    var button = $(this);
+    var formTag = button.parent();
+    var method = formTag.attr("method");
+    var url = formTag.attr("action");
+
+    $.ajax(url, {
+      type: method,
+      success: function(data){
+        showResults(data);
+      }
+    });
 
   });
+
+  function showResults(data) {
+    var list = $("<ul></ul>");
+    $("ul").replaceWith(list);
+    $("body").append(list);
+    for (var i = 0; i < data.length; i++) {
+      var listItem = $("<li></li>");
+      var anchor = $("<a></a>");
+      var result = data[i]
+      anchor.text(result.artist + ": " + result.title);
+      anchor.prop("href", result.url);
+      list.append(listItem);
+      $("li:last").append(anchor);
+    };
+  };
+
 });
