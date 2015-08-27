27d78d009f8305cb2365e29829de4c932af7b404
Sally Moore <sallyamoore@gmail.com>
2015-08-26 21:22:48 -0700

    Cleaned up code a bit. Now searching again without reloading replaces prior search results rather than adding to them. Added an if block in case artist is not found.

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -2,32 +2,26 @@ $(function() {
   $(".btn-search").click(function(event) {
     event.preventDefault();
 
-    var formTag = $(this).parent("form");
-    var artist = document.getElementById("artist").value; // $("#artist").val();
+    var artist = $(".artist").val();
     var url = ("/search/" + artist);
 
-    console.log(url);
     $.getJSON(url, function(data) {
-      for (var i = 0; i < data.length; i++) {
-        var thisArtist = data[i];
-        var listItem = $('<li></li>');
+      if (data.length === 0) {
+        // var video = $('<video></video>');
+        alert( "No one has heard of that artist! You must be the coolest!" );
+      } else {
+        $("li").remove();
+        for (var i = 0; i < data.length; i++) {
+          var thisArtist = data[i];
+          var listItem = $('<li></li>');
 
-        var anchor = $('<a></a>');
-        anchor.text(thisArtist.artist + ", " + thisArtist.title);
-        anchor.prop("href", thisArtist.url);
-        listItem.append(anchor);
-        $('ul').append(listItem);
+          var anchor = $('<a></a>');
+          anchor.text(thisArtist.artist + ", " + thisArtist.title);
+          anchor.prop("href", thisArtist.url);
+          listItem.append(anchor);
+          $('ul').append(listItem);
+        }
       }
-      // $.each(data, function(data) {
-        // var artistInfo = $("<a>data.artist</a>");
-        // artistInfo.prop("href", data.url);
-      // }
-      // error: function(data) {
-      //   console.log(errorThrown);
-      // },
-      // success: function(data) {
-      //   console.log(data);
-      // }
     }).fail(function() {
       alert( "error" );
     });
