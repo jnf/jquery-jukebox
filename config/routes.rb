235e2b44546fc1b6d5c91646c3e98a5f6f21959d
Lindsey Reno <lindseyannereno@gmail.com>
2015-08-27 15:42:29 -0700

    Added route for chance action

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "api#index"
   get '/search'  => 'api#search',  as: 'search'
   get '/popular' => 'api#popular', as: 'popular'
+  get '/chance'  => 'api#chance',  as: 'chance'
 end
