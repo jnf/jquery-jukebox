6a36145c8b9822d0730e9b73a5973097526f5bf0
Elia <elgrenier@gmail.com>
2015-08-27 14:11:34 -0700

    new route for new popular api

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   controller :api do
     get '/search', action: 'search', as: "search"
     get '/search/:artist', action: "search"
+    get "/popular", action: "popular"
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
