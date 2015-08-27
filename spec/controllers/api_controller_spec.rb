5fd0e550f8f4fe8cfce5ef641fc71136d4302740
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-08-27 16:08:12 -0700

    Adds tests for random API hit.

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 TIMJ_SEARCH   = { cassette_name: "TIMJ_search", record: :new_episodes }
 TIMJ_BREAKING = { cassette_name: "TIMJ_breaking", record: :new_episodes }
+TIMJ_RANDO = { cassette_name: "TIMJ_rando", record: :new_episodes }
 
 
 RSpec.describe ApiController, type: :controller do
@@ -55,4 +56,27 @@ RSpec.describe ApiController, type: :controller do
     end # context json
   end
 
+  describe "API Random", vcr: TIMJ_RANDO do
+    before :each do
+      get :rando
+    end
+
+    it "should be successful" do
+      expect(response).to be_ok
+    end
+
+    it "should return a json response object" do
+      expect(response.header['Content-Type']).to include 'application/json'
+    end
+
+    context "the returned json object" do
+      it "has the right keys" do
+        data = JSON.parse response.body
+
+        %w(title artist via url).each do |key|
+          expect(data.map(&:keys).flatten.uniq).to include key
+        end
+      end # it
+    end # context json
+  end
 end # describe
