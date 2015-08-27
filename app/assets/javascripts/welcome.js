f6e0c1c2ba747ea2c2a8d981fb5c39af96df51b4
Brenna <brennaleker@gmail.com>
2015-08-27 11:52:23 -0700

    switched if statement for hasselhoff vid to a ternary, yay ternaries

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -13,18 +13,14 @@ $(document).ready(function() {
       success: function (data) {
       clearPreviousResults();
         var songArray = data;
-        console.log(songArray);
-        if (songArray.length<1) {
-          $('body').append(defaultVideo);
-      } else {
+        (songArray.length < 1) ? noResults();
           for (var i = 0; i < songArray.length; i++){
-            var anchor = $('<a></a>');
-             anchor.text(songArray[i].title + " by " + songArray[i].artist);
-             anchor.prop('href', songArray[i].url);
-             $('body').append(anchor);
-             $('a').wrap( "<div class='result'></div>" )
-             $('body').append("<br>");
-          }
+          var anchor = $('<a></a>');
+           anchor.text(songArray[i].title + " by " + songArray[i].artist);
+           anchor.prop('href', songArray[i].url);
+           $('body').append(anchor);
+           $('a').wrap( "<div class='result'></div>" )
+           $('body').append("<br>");
         }
       }
     });
@@ -34,6 +30,15 @@ $(document).ready(function() {
 function clearPreviousResults(){
   $("a").remove();
   $("br").remove();
+  $("iframe").remove();
+  $("p").remove();
 };
 
-var defaultVideo = '<iframe width="420" height="315" src="https://www.youtube.com/embed/PJQVlVHsFF8" frameborder="0" allowfullscreen></iframe>'
+function noResults(){
+  $('body').append('<p>Unfortunately your search did not return any results, don\'t fret, David will keep you warm.</p>');
+  $('body').append('<br>');
+  $('body').append(defaultVideo);
+}
+
+
+var defaultVideo = '<iframe width="420" height="315" src="https://www.youtube.com/embed/PJQVlVHsFF8?autoplay=1" frameborder="0" allowfullscreen></iframe>'
