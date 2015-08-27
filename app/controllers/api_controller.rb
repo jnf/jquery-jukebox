afb9ae005a1afd9fa4c4b5cc9a5b3671ab25b8cf
Lindsey Reno <lindseyannereno@gmail.com>
2015-08-27 14:16:21 -0700

    Added popular action and popular jams endpoint

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,11 +1,25 @@
 class ApiController < ApplicationController
   JAM = "http://api.thisismyjam.com/1/search/jam.json"
+  POPULAR = "http://api.thisismyjam.com/1/explore/popular.json"
 
   def index; end
 
   def search
     begin
       response = HTTParty.get(JAM, query: { "by" => "artist", "q" => params[:artist] })
+      data = setup_data(response)
+      code = :ok
+    rescue
+      data = {}
+      code = :no_content
+    end
+
+    render json: data.as_json, code: code
+  end
+
+  def popular
+    begin
+      response = HTTParty.get(POPULAR)
       data = setup_data(response)
       code = :ok
     rescue
