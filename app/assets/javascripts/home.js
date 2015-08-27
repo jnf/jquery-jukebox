4074b11862aa65cae55e2fc9102744f92c9ca151
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-08-27 13:54:36 -0700

    Just about to start on Wave 2. Messing around with styling.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,82 +1,48 @@
 $(function () {
-
-  // var player;
-  // function onYouTubeIframeAPIReady() {
-  //   player = new YT.Player('player', {
-  //     height: '390',
-  //     width: '640',
-  //     videoId: 'M7lc1UVf-VE',
-  //     events: {
-  //       'onReady': onPlayerReady,
-  //       'onStateChange': onPlayerStateChange
-  //     }
-  //   });
-  // }
-  //
-  // document.body.appendChild(onYouTubeIframeAPIReady());
-
+  // WAS TESTING CREATING AN ATTRIBUTE IN JAVASCRIPT
   function addAttr() {
     var button = document.getElementsByTagName("input");
+    // gets the last possible attribute that is an input
     var button = button[(button.length - 1)];
-    var att = document.createAttribute("testing");
+    var att = document.createAttribute("search");
     att.value = "yes";
     button.setAttributeNode(att);
-
-    // var embededThang = document.createElement("embed");
-    // embededThang.setAttribute("src", "http://images.freeimages.com/images/previews/5b0/rubik-s-cube-1-1424892.jpg");
-    // document.body.appendChild(embededThang);
   };
   addAttr();
 
-  // $('input[type=submit]').click(function(event) {
-  $('input[testing=yes]').click(function(event) {
+  $('input[search=yes]').click(function(event) {
     event.preventDefault();
 
     var button = $(this);
     var box = button.siblings('input[type=text]');
-    // console.log(box);
-    // console.log(box.val());
     var parent = button.parent();
-    // console.log(parent);
 
-    var url = 'http://localhost:3000/search/' + box.val();
+    var url = '/search/' + box.val();
     var method = parent.attr("method");
 
-    $(".results").empty();
+    $(".results").empty(); // empties the previous results
     $.ajax(url, {
       type: method,
       success: function (data) {
-        // console.log(data);
 
         // To add unordered list tags around all the list items.
         var unorderedList = $("<ul></ul>");
         results = $('.results')
         results.append(unorderedList);
         insideList = results.children(':first-child');
-        // console.log(insideList);
 
         // To add all the list items for all the results.
         for (var i = 0; i < data.length; i++) {
           var anchor = $("<a></a>"); //new anchor element
           anchor.text(data[i].title);
           anchor.prop("href", data[i].url);
-            //attr vs prop -- look up the difference
-            //href belongs to anchors
-          //   prop is short for property
+            // attr vs prop -- look up the difference
+            // prop is short for property
           var listItem = $("<li></li>");
           listItem.html(anchor);
-          // var breaking = $("<br>"); //new anchor element
-          // $('body').append(anchor); //=> <a href="link/to/stuff">Text on the page</a>
-          // $('body').append(breaking);
           insideList.append(listItem);
-          // console.log(listItem);
-          // var test = unorderedList.html;
 
-          // unorderedList.html(test + listItem);
-            //append vs prepend
-            //replace (which will replace it with this node)
-            //html <-- can replace raw HTML with other nodes
-
+          // Adding the embeded video.
           var urlString = data[i].url;
           if (urlString.search("youtube") != -1) {
             var embededThang = $("<iframe></iframe>");
@@ -89,16 +55,11 @@ $(function () {
             urlString = urlString + "?enablejsapi=1";
             console.log(urlString);
             embededThang.prop("src", urlString);
-            // embededThang.setAttribute("src", data[i].url);
+              // embededThang.setAttribute("src", data[i].url);
             embededThang.prop("frameborder", "0");
             insideList.append(embededThang);
           }
-            //
-            // <iframe id="player" type="text/html" width="640" height="390"
-            // src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
-            // frameborder="0"></iframe>
         }
-        // $('body').append(unorderedList);
       }
     });
   });
