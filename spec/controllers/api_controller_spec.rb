9e581cc69bd8179943f5e5309b87e26acb296c4b
Lindsey Reno <lindseyannereno@gmail.com>
2015-08-27 14:59:57 -0700

    Added tests for the chance endpoint

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 TIMJ_SEARCH = { cassette_name: "TIMJ_search", record: :new_episodes }
 POPULAR = { cassette_name: "Popular", record: :new_episodes }
+CHANCE = { cassette_name: "Chance", record: :new_episodes }
 
 RSpec.describe ApiController, type: :controller do
   describe "interacting with the This Is My Jam API" do
@@ -47,6 +48,50 @@ RSpec.describe ApiController, type: :controller do
           data = JSON.parse response.body
           %w(title artist via url).each do |key|
             expect(data.map(&:keys).flatten.uniq).to include key
+          end
+        end
+      end
+    end
+
+    describe "exploring chance", vcr: CHANCE do
+      before :each do
+        get :search
+      end
+
+      context "when nothing is typed into the search field" do
+        it "should be successful" do
+          expect(response).to be_ok
+        end
+
+        it "should return a json response object" do
+          expect(response.header['Content-Type']).to include 'application/json'
+        end
+
+        context "the returned json object" do
+          it "has the right keys" do
+            data = JSON.parse response.body
+            %w(title artist via url).each do |key|
+              expect(data.map(&:keys).flatten.uniq).to include key
+            end
+          end
+        end
+      end
+
+      context "when the search has no results" do
+        it "should be successful" do
+          expect(response).to be_ok
+        end
+
+        it "should return a json response object" do
+          expect(response.header['Content-Type']).to include 'application/json'
+        end
+
+        context "the returned json object" do
+          it "has the right keys" do
+            data = JSON.parse response.body
+            %w(title artist via url).each do |key|
+              expect(data.map(&:keys).flatten.uniq).to include key
+            end
           end
         end
       end
