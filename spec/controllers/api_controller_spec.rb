5d17e1765adf3ab7b32d4488c60ffd4a22b4b92d
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-08-28 16:02:10 -0700

    Project stuff.

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 TIMJ_SEARCH = { cassette_name: "TIMJ_search", record: :new_episodes }
+TIMJ_SEARCH_NO = { cassette_name: "TIMJ_search_no", record: :new_episodes }
+TIMJ_RANDOM = { cassette_name: "TIMJ_random", record: :new_episodes }
 
 RSpec.describe ApiController, type: :controller do
-  describe "interacting with the This Is My Jam API", vcr: TIMJ_SEARCH do
+  describe "interacting with the This Is My Jam API search", vcr: TIMJ_SEARCH do
     context "#search" do
       before :each do
         get :search, artist: "the knife"
@@ -27,7 +29,28 @@ RSpec.describe ApiController, type: :controller do
         end
       end
     end
+  end
 
+  describe "interacting with the This Is My Jam API search no results", vcr: TIMJ_SEARCH_NO do
+    context "#search (no results)" do
+      before :each do
+        get :search, artist: "398572938doijfw"
+      end
+
+      it "should be successful" do
+        expect(response.status).to eq 204
+      end
+      
+      context "the returned json object" do
+        it "is empty" do
+          data = JSON.parse response.body
+          expect(data).to eq []
+        end
+      end
+    end
+  end
+
+  describe "interacting with the This Is My Jam API random", vcr: TIMJ_RANDOM do
     context "#random" do
       before :each do
         get :random
