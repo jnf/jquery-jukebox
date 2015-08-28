242ebc2c04408c18aeeada7b73def6c47182d5ca
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-28 09:26:37 -0700

    uncommented all :unpopular tests

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -77,19 +77,19 @@ RSpec.describe ApiController, type: :controller do
         expect(response).to be_ok
       end
 
-      # it "should return a json response object" do
-      #   expect(response.header['Content-Type']).to include 'application/json'
-      # end
+      it "should return a json response object" do
+        expect(response.header['Content-Type']).to include 'application/json'
+      end
 
-      # context "the returned json object" do
-      #   it "has the right keys" do
-      #     data = JSON.parse response.body
+      context "the returned json object" do
+        it "has the right keys" do
+          data = JSON.parse response.body
           
-      #     %w(title artist via url).each do |key|
-      #       expect(data.map(&:keys).flatten.uniq).to include key
-      #     end
-      #   end
-      # end
+          %w(title artist via url).each do |key|
+            expect(data.map(&:keys).flatten.uniq).to include key
+          end
+        end
+      end
     end
   end
 end
