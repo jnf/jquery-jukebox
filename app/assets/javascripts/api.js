a1c490e15e2b7e50f788b14113f18480e2f17315
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-08-27 17:20:55 -0700

    Adds button hover class.

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -7,14 +7,13 @@ $(function() { // autoloads javascript into browser window for the rails window
 
     var formTag = $(this).parent();
     var artist = $('input[type="text"]').val();
+    var url = formTag.attr('action') + "/" + artist;
+    var method = formTag.attr('method');
 
     if (artist.length == 0) {  // guard for if user has no input
       alert("Please enter an artist");
       return;
     }
-
-    var url = formTag.attr('action') + "/" + artist;
-    var method = formTag.attr('method');
 
     $.ajax(url, {
       type: method,
@@ -22,12 +21,8 @@ $(function() { // autoloads javascript into browser window for the rails window
         var list = $('<ul></ul>')
 
         if (data.length == 0) {
-
-        var listItem = $('<li></li>'); // new list element
-        var anchor = $('<a></a>'); // new anchor element
-          list.append('<iframe width="420" height="315" src="https://www.youtube.com/embed/KmtzQCSh6xk" frameborder="0" allowfullscreen></iframe>')
+          defaultResult();
         } else {
-
           for(var i = 0; i < data.length-1; i ++) {
             result(i, data, list);
         }
@@ -48,7 +43,6 @@ $(function() { // autoloads javascript into browser window for the rails window
     // overrides default behavior normally associated with choose button
 
     var formTag = $(this);
-
     var url = formTag.attr('action');
     var method = formTag.attr('method');
 
@@ -74,7 +68,6 @@ $(function() { // autoloads javascript into browser window for the rails window
    // overrides default behavior normally associated with choose button
 
    var formTag = $(this);
-
    var url = formTag.attr('action');
    var method = formTag.attr('method');
 
@@ -108,5 +101,11 @@ $(function() { // autoloads javascript into browser window for the rails window
     listItem.append(anchor[0]); // link
     list.append(listItem);
   };
+
+  function defaultResult() {
+    var listItem = $('<li></li>'); // new list element
+    var anchor = $('<a></a>'); // new anchor element
+    list.append('<iframe width="420" height="315" src="https://www.youtube.com/embed/KmtzQCSh6xk" frameborder="0" allowfullscreen></iframe>');
+  }
 
 }); // end
