bd96a139f23582841a6a16d86da5742c04b0bb78
Corinne Pingul <corinnepingul@gmail.com>
2015-08-28 13:06:20 -0700

    adds search and random functions

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,9 +1,25 @@
 class ApiController < ApplicationController
-  JAM = "http://api.thisismyjam.com/1/search/jam.json"
+  JAM_SEARCH = "http://api.thisismyjam.com/1/search/jam.json"
+  JAM_RANDOM = "http://api.thisismyjam.com/1/explore/chance.json"
+
+  def index; end
 
   def search
     begin
-      response = HTTParty.get(JAM, query: { "by" => "artist", "q" => params[:artist] })
+      response = HTTParty.get(JAM_SEARCH, query: { "by" => "artist", "q" => params[:artist] })
+      data = setup_data(response)
+      code = :ok
+    rescue
+      data = {}
+      code = :no_content
+    end
+
+    render json: data.as_json, code: code
+  end
+
+  def random
+    begin
+      response = HTTParty.get(JAM_RANDOM)
       data = setup_data(response)
       code = :ok
     rescue
@@ -23,7 +39,8 @@ class ApiController < ApplicationController
         via: jam.fetch("via", ""),
         url: jam.fetch("viaUrl", ""),
         title: jam.fetch("title", ""),
-        artist: jam.fetch("artist", "")
+        artist: jam.fetch("artist", ""),
+        thumbnail: jam.fetch("jamvatarMedium", "")
       }
     end
   end
