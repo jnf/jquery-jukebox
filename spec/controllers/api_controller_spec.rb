61d80e2553cf230ee3eb5b7c2d2d9ab3b0695d5e
Lindsey Reno <lindseyannereno@gmail.com>
2015-08-28 10:26:31 -0700

    Added tests for rare action

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 TIMJ_SEARCH = { cassette_name: "TIMJ_search", record: :new_episodes }
-POPULAR = { cassette_name: "Popular", record: :new_episodes }
-CHANCE = { cassette_name: "Chance", record: :new_episodes }
+POPULAR     = { cassette_name: "Popular", record: :new_episodes }
+CHANCE      = { cassette_name: "Chance", record: :new_episodes }
+RARE        = { cassette_name: "Rare", record: :new_episodes }
 
 RSpec.describe ApiController, type: :controller do
   describe "interacting with the This Is My Jam API" do
@@ -54,6 +55,29 @@ RSpec.describe ApiController, type: :controller do
     end
 
     describe "exploring chance", vcr: CHANCE do
+      before :each do
+        get :chance
+      end
+
+      it "should be successful" do
+        expect(response).to be_ok
+      end
+
+      it "should return a json response object" do
+        expect(response.header['Content-Type']).to include 'application/json'
+      end
+
+      context "the returned json object" do
+        it "has the right keys" do
+          data = JSON.parse response.body
+          %w(title artist via url).each do |key|
+            expect(data.map(&:keys).flatten.uniq).to include key
+          end
+        end
+      end
+    end
+
+    describe "exploring rare", vcr: RARE do
       before :each do
         get :chance
       end
