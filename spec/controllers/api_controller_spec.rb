31567b275d56b15c4df2ea4d6180c6e6ebf96842
Loraine <loraine@lorainekv.com>
2015-08-28 09:47:15 -0700

    Tests for TIMJ popular songs api endpoint.

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 TIMJ_SEARCH = { cassette_name: "TIMJ_search", record: :new_episodes }
+TIMJ_POP = { cassette_name: "TIMJ_pop", record: :new_episodes }
+
 
 RSpec.describe ApiController, type: :controller do
-  describe "interacting with the This Is My Jam API", vcr: TIMJ_SEARCH do
+  describe "searching for artists with the This Is My Jam API", vcr: TIMJ_SEARCH do
     before :each do
       get :search, artist: "the knife"
     end
@@ -19,11 +21,36 @@ RSpec.describe ApiController, type: :controller do
     context "the returned json object" do
       it "has the right keys" do
         data = JSON.parse response.body
-        
+
         %w(title artist via url).each do |key|
           expect(data.map(&:keys).flatten.uniq).to include key
         end
       end
     end
   end
+
+  describe "getting popular songs from This Is My Jam API", vcr: TIMJ_POP do
+    before :each do
+      get :popular
+    end
+
+    it "should be successful" do
+      expect(response).to be_ok
+    end
+
+    it "should return a json response object" do
+      expect(response.header['Content-Type']).to include 'application/json'
+    end
+
+    context "the returned json object" do
+      it "has the right keys" do
+        data = JSON.parse response.body
+
+        %w(title artist via url).each do |key|
+          expect(data.map(&:keys).flatten.uniq).to include key
+        end
+      end
+    end
+  end
+
 end
