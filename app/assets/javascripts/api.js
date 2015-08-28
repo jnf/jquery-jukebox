d9441e50c2059278d993ae9b483fb13d17b3491c
Lindsey Reno <lindseyannereno@gmail.com>
2015-08-27 19:04:38 -0700

    Added logic to embed youtube and vimeo videos

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -6,15 +6,17 @@ $(function() {
     var formTag = button.parent();
     var method = formTag.attr("method");
     var url = formTag.attr("action");
+    var chanceForm = $("form[action='/chance']")
+    var chanceMethod = chanceForm.attr("method")
+    var chanceUrl = chanceForm.attr("action")
 
     $.ajax(url, {
       type: method,
       data: { 'artist' : $("#artist").val() },
       success: function(data) {
         if (data.length == 0){
-          $.ajax({
-            url: '/chance',
-            type:'get',
+          $.ajax(chanceUrl, {
+            type: chanceMethod,
             success: function(data) {
               console.log(data);
               showResults(data);
@@ -67,13 +69,31 @@ $(function() {
     $("ul").replaceWith(list);
     $("body").append(list);
     for (var i = 0; i < data.length; i++) {
-      var listItem = $("<li></li>");
-      var anchor = $("<a></a>");
       var result = data[i]
-      anchor.text(result.artist + ": " + result.title);
-      anchor.prop("href", result.url);
-      list.append(listItem);
-      $("li:last").append(anchor);
+      var listItem = $("<li></li>");
+      if (result.via == "youtube"){
+        var iframe = $("<iframe></iframe>");
+        var urlResult = result.url;
+        var youtubeId = urlResult.substr(urlResult.indexOf('=') + 1);
+        var src = "http://www.youtube.com/embed/" + youtubeId;
+        iframe.prop("src", src);
+        list.append(listItem);
+        $("li:last").append(iframe);
+      } else if (result.via == "vimeo"){
+          var iframe = $("<iframe></iframe>");
+          var urlResult = result.url;
+          var vimeoId = urlResult.substr(urlResult.lastIndexOf('/') + 1);
+          var src = "http://player.vimeo.com/video/" + vimeoId;
+          iframe.prop("src", src);
+          list.append(listItem);
+          $("li:last").append(iframe);
+      } else {
+        var anchor = $("<a></a>");
+        anchor.text(result.artist + ": " + result.title);
+        anchor.prop("href", result.url);
+        list.append(listItem);
+        $("li:last").append(anchor);
+      }
     };
   };
 
