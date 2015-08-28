86376e7ac647f82c3c17710984881369d638846b
Loren Wang <wang131.131@gmail.com>
2015-08-27 22:41:35 -0700

    tests for new RANDO (Chance) api endpoint. done with Wave 2

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 TIMJ_SEARCH = { cassette_name: "TIMJ_search", record: :new_episodes }
+RANDO = { cassette_name: "Rando", record: :new_episodes }
 
 RSpec.describe ApiController, type: :controller do
   describe "interacting with the This Is My Jam API", vcr: TIMJ_SEARCH do
@@ -19,7 +20,31 @@ RSpec.describe ApiController, type: :controller do
     context "the returned json object" do
       it "has the right keys" do
         data = JSON.parse response.body
-        
+
+        %w(title artist via url).each do |key|
+          expect(data.map(&:keys).flatten.uniq).to include key
+        end
+      end
+    end
+  end
+
+  describe "rando", vcr: RANDO do
+    before :each do
+      get :rando
+    end
+
+    it "should be successful" do
+      expect(response).to be_ok
+    end
+
+    it "should return a json response object" do
+      expect(response.header['Content-Type']).to include 'application/json'
+    end
+
+    context "the returned json object" do
+      it "has the right keys" do
+        data = JSON.parse response.body
+
         %w(title artist via url).each do |key|
           expect(data.map(&:keys).flatten.uniq).to include key
         end
