0311f1bf5fb466b492959ca753512b3eb7d5218b
Sally Moore <sallyamoore@gmail.com>
2015-08-27 19:30:36 -0700

    Fixed display problem and did a bit of styling.

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -4,6 +4,7 @@ $(function() {
   var failedRandoMessage = "Our randomizer broke! Try again.";
   var errorMsg = "Something terrible has happened. Run for the hills.";
   var startIndex = 1;
+  var sweep = "li, iframe, a, .btn-play, .glyphicon-play, .message, .first-song"
 
   function noData(message) {
     var frame = $("<iframe id='frame' width='640' height='360' src='' frameborder='0' autoplay='1' allowfullscreen></iframe>");
@@ -33,36 +34,33 @@ $(function() {
     for (var i = startIndex; i < data.length; i++) {
       var thisArtist = data[i];
       var id = "song" + i;
-      var listItem = $('<div class="btn-group" id=' + id + ' role="group"><li></li></div>');
-      var anchor = $('<a type="button" class="btn btn-default"></a>');
+      var listItem = $('<li class="list-item"></li>');
+      var anchor = $('<a class="col-sm-10"></a>');
 
       anchor.text(thisArtist.artist + ", " + thisArtist.title);
       anchor.prop("href", thisArtist.url);
-      listItem.append(anchor);
+      $(listItem).append(anchor);
 
-      var glyph = $('<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-play"></span></button><br>');
-      listItem.append(glyph);
-
+      if (thisArtist.via === "youtube") {
+        var glyph = $('<button class="col-sm-1 btn btn-play" id=' + id + '><span class="glyphicon glyphicon-play"></span></button>');
+        listItem.append(glyph);
+      }
       $('ul').append(listItem);
-
       youtubeOnClick(thisArtist, id);
     }
   }
 
   // On click, show youtube
   function youtubeOnClick(song, id) {
-
     $("#" + id).click(function(event) {
       event.preventDefault();
-      if (song.via === "youtube") {
+
         $("iframe").remove();
-        // showYoutube(song.url);
 
         var frame = $("<iframe id='frame' width='448' height='252' src='' frameborder='0' allowfullscreen></iframe><br>");
         $("#" + id).append(frame);
         song.url = song.url.replace("watch?v=", "/embed/");
         frame.attr("src", song.url + "?rel=0");
-      }
     });
   }
 
@@ -74,7 +72,7 @@ $(function() {
     var url = ("/search/" + artist);
 
     $.getJSON(url, function(data) {
-      $("li, iframe, .message, .first-song").remove();
+      $(sweep).remove();
       if (data.length === 0) {
         noData(noArtistMessage);
       } else {
@@ -96,7 +94,7 @@ $(function() {
     var url = ("/chance");
 
     $.getJSON(url, function(data) {
-      $("li, iframe, .message, .first-song").remove();
+      $(sweep).remove();
 
       if (data.length === 0) {
         noData(failedRandoMessage);
