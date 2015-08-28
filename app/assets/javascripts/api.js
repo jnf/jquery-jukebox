e91a3651f08564abd4bcdd91ac0a1badf6bd0ad0
Loraine <loraine@lorainekv.com>
2015-08-27 18:07:15 -0700

    WIP refactoring

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -1,59 +1,62 @@
 
 $(function() {
 
-  // Sets search form values
-  function setSearchVals(){
-    var artistField = $("input#artist");
-    var query = artistField.val();
-    var formTag = $("form#search");
-    var url = formTag.attr("action");
-    var method = formTag.attr("method");
-
-    return {
-      // This needs to be a key value pair
-      params : { "artist": query},
-      url : url,
-      method: method
-    };
-  }
-
-  function executeSearch(searchVals){
-    $.ajax(searchVals.url, {
-      type: searchVals.method,
-      data: searchVals.params,
-      success: function (data) {
-        var artistArray = data; // Renamed for clarity
-
-        // Removing the previous results
-        $("a, iframe").remove();
-
-        // For each search result
-        for (i = 0; i < artistArray.length; i++) {
-
-          // YouTube embed
-          if (artistArray[i].via == "youtube") {
-            var iframeTemplate = $("<iframe class='other-media' type='text/html' width='640' height='390' src = '' ></iframe>");
-            var youtubeURL = artistArray[i].url;
-            var modifiedYoutubeUrl = youtubeURL.replace("watch?v=", "embed/");
-            iframeTemplate.attr("src", modifiedYoutubeUrl);
-            $(".search-results").append(iframeTemplate);
-          } else {
-            //  Link Version
-            var anchor = $("<a class='song-link' target='new'></a>");
-            anchor.text(artistArray[i].artist + " - " + artistArray[i].title);
-            anchor.prop("href", artistArray[i].url);
-            $(".search-results").append(anchor);
-          }
-        }
-
-        $(".song-link").wrap("<p></p>");
-
-      }
-    });
-  }
-
   $(".search").click(function(event){
+
     event.preventDefault();
+
+    function setSearchVals(){
+      var artistField = $("input#artist");
+      var query = artistField.val();
+      var formTag = $("form#search");
+      var url = formTag.attr("action");
+      var method = formTag.attr("method");
+
+      return {
+        // This needs to be a key value pair
+        params : { "artist": query},
+        url : url,
+        method: method
+      };
+    }
+
+    function executeSearch(searchVals){
+      $.ajax(searchVals.url, {
+        type: searchVals.method,
+        data: searchVals.params,
+        success: function (data) {
+          $("a, iframe").remove();
+
+          // refactor this shizzz
+          
+          var artistArray = data; // Renamed for clarity
+
+          // Removing the previous results
+
+          // For each search result
+          for (i = 0; i < artistArray.length; i++) {
+
+            // YouTube embed
+            if (artistArray[i].via == "youtube") {
+              var iframeTemplate = $("<iframe class='other-media' type='text/html' width='640' height='390' src = '' ></iframe>");
+              var youtubeURL = artistArray[i].url;
+              var modifiedYoutubeUrl = youtubeURL.replace("watch?v=", "embed/");
+              iframeTemplate.attr("src", modifiedYoutubeUrl);
+              $(".search-results").append(iframeTemplate);
+            } else {
+              //  Link Version
+              var anchor = $("<a class='song-link' target='new'></a>");
+              anchor.text(artistArray[i].artist + " - " + artistArray[i].title);
+              anchor.prop("href", artistArray[i].url);
+              $(".search-results").append(anchor);
+            }
+          }
+
+          $(".song-link").wrap("<p></p>");
+
+        }
+      });
+    }
 
     executeSearch(setSearchVals())
 
