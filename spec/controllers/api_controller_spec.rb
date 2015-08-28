2c7d400592f52798596569c680708ef2d507f07c
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-28 09:45:14 -0700

    added tests for breaking jams

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -6,6 +6,7 @@ TIMJ_SEARCH_EMPTY = { cassette_name: "TIMJ_no_content", record: :new_episodes }
 TIMJ_POPULAR = { cassette_name: "TIMJ_POPULAR", record: :new_episodes }
 TIMJ_UNPOPULAR = { cassette_name: "TIMJ_UNPOPULAR", record: :new_episodes }
 TIMJ_RANDO = { cassette_name: "TIMJ_RANDO", record: :new_episodes }
+TIMJ_BREAKING = { cassette_name: "TIMJ_BREAKING", record: :new_episodes }
 
 RSpec.describe ApiController, type: :controller do
   describe "Searching for JAMS", vcr: TIMJ_SEARCH do
@@ -97,6 +98,32 @@ RSpec.describe ApiController, type: :controller do
     context "Got many results" do
       before :each do
         get :rando
+      end
+
+      it "should be successful" do
+        expect(response).to be_ok
+      end
+
+      it "should return a json response object" do
+        expect(response.header['Content-Type']).to include 'application/json'
+      end
+
+      context "the returned json object" do
+        it "has the right keys" do
+          data = JSON.parse response.body
+          
+          %w(title artist via url).each do |key|
+            expect(data.map(&:keys).flatten.uniq).to include key
+          end
+        end
+      end
+    end
+  end
+
+describe "Requesting BREAKING jams", vcr: TIMJ_BREAKING do
+    context "Got many results" do
+      before :each do
+        get :breaking
       end
 
       it "should be successful" do
