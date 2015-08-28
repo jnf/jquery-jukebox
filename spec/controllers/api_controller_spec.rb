d782019c19e18617cc01ccc5acd3f3962e5deed9
Corinne Pingul <corinnepingul@gmail.com>
2015-08-28 13:08:48 -0700

    Adds tests for random api search functionality.

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 TIMJ_SEARCH = { cassette_name: "TIMJ_search", record: :new_episodes }
+TIMJ_RANDOM = { cassette_name: "TIMJ_random" }
 
 RSpec.describe ApiController, type: :controller do
-  describe "interacting with the This Is My Jam API", vcr: TIMJ_SEARCH do
+  describe "interacting with the This Is My Jam API: Search", vcr: TIMJ_SEARCH do
     before :each do
       get :search, artist: "the knife"
     end
@@ -19,10 +20,32 @@ RSpec.describe ApiController, type: :controller do
     context "the returned json object" do
       it "has the right keys" do
         data = JSON.parse response.body
-        
-        %w(title artist via url).each do |key|
+
+        %w(title artist via url thumbnail).each do |key|
           expect(data.map(&:keys).flatten.uniq).to include key
         end
+      end
+    end
+  end
+
+  describe "interacting with the This Is My Jam API: Random", vcr: TIMJ_RANDOM do
+    before :each do
+      get :random
+    end
+
+    it "should be successful" do
+      expect(response).to be_ok
+    end
+
+    it "should return a json response object" do
+      expect(response.header['Content-Type']).to include 'application/json'
+    end
+
+    it "has the right keys" do
+      data = JSON.parse response.body
+
+      %w(title artist via url thumbnail).each do |key|
+        expect(data.map(&:keys).flatten.uniq).to include key
       end
     end
   end
