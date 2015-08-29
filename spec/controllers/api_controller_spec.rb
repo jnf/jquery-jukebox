9eaafd7b0e248e1b8b4bd7dabcb889b9c6ad8902
Ashley Watkins <AWatkins31@gmail.com>
2015-08-28 21:08:00 -0700

    Adds tests.
    I should've spent more time on this part. There are a lot of things to learn here. I focused on JS instead, thoug =/

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
+# TODO: What does 'record: :new_episodes' do??
 TIMJ_SEARCH = { cassette_name: "TIMJ_search", record: :new_episodes }
 TIMJ_SEARCH_EMPTY = { cassette_name: "TIMJ_no_content", record: :new_episodes }
 
@@ -21,7 +22,7 @@ RSpec.describe ApiController, type: :controller do
       context "the returned json object" do
         it "has the right keys" do
           data = JSON.parse response.body
-          
+
           %w(title artist via url).each do |key|
             expect(data.map(&:keys).flatten.uniq).to include key
           end
@@ -33,6 +34,32 @@ RSpec.describe ApiController, type: :controller do
       it "tells me I've got a 204" do
         get :search, artist: "lololololololol"
         expect(response.status).to eq 204
+      end
+    end
+  end
+
+  describe "GET #random", vcr: TIMJ_SEARCH do
+    context "Got many results" do
+      before :each do
+          get :random
+      end
+
+      it "should be successful" do
+        expect(response).to be_ok
+      end
+
+      it "should return a json response object" do
+        expect(response.header['Content-Type']).to include 'application/json'
+      end
+
+      context "the returned json object" do
+        it "has the right keys" do
+          data = JSON.parse response.body
+
+          %w(title artist via url).each do |key|
+            expect(data.map(&:keys).flatten.uniq).to include key
+          end
+        end
       end
     end
   end
