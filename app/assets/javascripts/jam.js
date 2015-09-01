ca0f58f9e79cb7fdfd6ecf41295ca7fe265dd2da
jnf <jeremy.flores@gmail.com>
2015-09-01 08:35:28 -0700

    Some refactor to support random jams.

--- app/assets/javascripts/jam.js
+++ app/assets/javascripts/jam.js
@@ -1,4 +1,13 @@
 $('document').ready(function() {
+  $('.popular_jams').click(function(event) {
+    event.preventDefault();
+
+    console.log('popular');
+    $.get("http://api.thisismyjam.com/1/explore/popular.json", function(response) {
+      console.log(response)
+    })
+  });
+
   $('#artist-search').submit(function(event) {
     event.preventDefault();
     var artist = $(this).find('input[name=artist]').val();
@@ -16,60 +25,3 @@ $('document').ready(function() {
     }
   });  
 });
-
-var Formatter = function(data) {
-  this.data = data;
-};
-
-Formatter.prototype.build = function() {
-  switch(this.data["via"]) {
-    case "youtube":
-      var code = this.youtube_embed();
-      break;
-    case "vimeo":
-      var code = this.vimeo_embed();
-      break;
-    case "soundcloud":
-      var code = this.soundcloud_embed();
-      break;
-    default:
-      var code = this.link_to();
-  }
-
-  return $('<li></li>').html(code);  
-}
-
-Formatter.prototype.link_to = function() {
-  var anchor = $('<a></a>');
-  
-  anchor.prop('href', this.data["url"]);
-  anchor.text(this.data["artist"] + ": " + this.data["title"]);
-  return anchor;
-}
-
-Formatter.prototype.youtube_embed = function() {
-  var embed_reference = this.data["url"].split("=")[1];
-  var iframe = $('<iframe></iframe>');
-
-  iframe.prop("allowfullscreen","").prop('frameborder', 0);
-  iframe.prop("src", "https://www.youtube.com/embed/" + embed_reference);
-  return iframe;
-}
-
-Formatter.prototype.vimeo_embed = function() {
-  var embed_reference = this.data["url"].split("/").pop();
-  var iframe = $('<iframe></iframe>');
-
-  iframe.prop("allowfullscreen","").prop('frameborder', 0);
-  iframe.prop("src", "https://player.vimeo.com/video/" + embed_reference);
-  return iframe;
-}
-
-Formatter.prototype.soundcloud_embed = function() {
-  var embed_reference = encodeURI(this.data["url"]);
-  var iframe = $('<iframe></iframe>');
-
-  iframe.prop("allowfullscreen","").prop('frameborder', 0);
-  iframe.prop("src", "https://w.soundcloud.com/player/?url=" + embed_reference);
-  return iframe;
-}
